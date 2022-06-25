50b6b0ac7b8f1cb115a92ad8fd1bcf6fde03a97f
GeanCR <geancarloscolpas@gmail.com>
2022-06-25 12:24:54 -0500

    Tarea1homwork1

---
+++ 02-JS-I/homework/Homworktext-1.js
@@ -0,0 +1,6 @@
+// variables: Es la forma en como guardamos un valor con un nombre especifico.
+// Strings: Es un tipo de datos que consta de un bloque de texto siempre entre comillas
+// Funciones: Son tipo particulares de objetos que tienen la misma propiedad que cualquier objeto,
+// Esta antecedida de la palabra Function y tiene un nombre que describe lo que hace
+// Funciones (Argumentos) Es un tipo de datos que se genera al interior de cada funcion recogiendo todos los parametros que se an enviado a la misma.
+// Valores Booleanos: Son valores que provienen de la logica de Boole; Significa que puede tener un valor de dos opciones (Verdadero o Falso)
