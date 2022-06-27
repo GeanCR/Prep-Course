68346ac2cdc2e4c542d5426ad97045726e407395
GeanCR <geancarloscolpas@gmail.com>
2022-06-27 11:35:53 -0500

    tarea teorica

---
+++ 03-JS-II/homework/Untitled-1.js
@@ -0,0 +1,3 @@
+// Bucle For: Si queremos que un ejercicio se repita muchas veces y conocemos la cantidad de veces que se repita podemos hacer 
+// uso del For; no nos permite que un ejercicio sea infinito.
+// Los operadores logicos &&; ||; !; nos permiten evaluar 2 expresiones al mismo tiempo y devolvernos un valor Booleano con respecto a cada una de las expresiones.
